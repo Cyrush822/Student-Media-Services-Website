c7b26765bb7c490b8905e235ec03e14d39834e9a
Alan <alan2021233@users.noreply.github.com>
2020-11-03 12:09:09 +0800

    Corrected emails in emailheader.php

--- website_all_files/emailheader.php
+++ website_all_files/emailheader.php
@@ -1,42 +1,52 @@
-<?php 
+<?php
+
+include 'includes/email_username_password.php';
 
 
-$very_secure_email_username = "sms@concordiashanghai.org";                 // SMTP username
-$very_secure_email_password = "SMSConcordiaSH2017";  
+//Emailer names:
+$name_of_emailing_person = 'Rinka -  The Projects and Service Manager of SMS';
 
 //Exec Role Emails
 
-$project_manager_email = 'Nicholas2019108@concordiashanghai.org';
+$supervisor_email = 'brian.lavender@concordiashanghai.org';
 
-$organizer_email = 'Nicholas2019108@concordiashanghai.org';
+$project_manager_email = 'rinka2021219@concordiashanghai.org';
 
-$digital_makerspace_email = 'Nicholas2019108@concordiashanghai.org';
 
-$marketing_email = 'Nicholas2019108@concordiashanghai.org';
+$organizer_email = 'eunice2022322@concordiashanghai.org';
+$organizer_email2 = 'henie2021341@concordiashanghai.org';
+
+$digital_makerspace_email = 'cindy2021260@concordiashanghai.org';
+$digital_makerspace_email2 = 'allison2022089@concordiashanghai.org';
+
+$marketing_email = 'andrew2021199@concordiashanghai.org';
+$marketing_email2 = 'athena2021191@concordiashanghai.org';
 
 
 
 //Sig Leader Emails
-$principia_email = 'Nicholas2019108@concordiashanghai.org';
 
-$livestream_email = 'Nicholas2019108@concordiashanghai.org';
+$livestream_email = 'geon2022370@concordiashanghai.org';
+$livestream_email2 = 'Jaeryun2022208@concordiashanghai.org';
 
-$visual_design_email = 'Nicholas2019108@concordiashanghai.org';
+$visual_design_email2 = 'andrew2021199@concordiashanghai.org';
+$visual_design_email3 = 'athena2021191@concordiashanghai.org';
 
-$united_herald_email = 'Nicholas2019108@concordiashanghai.org';
+$united_herald_email = 'alisa2021166@concordiashanghai.org';
+$united_herald_email2 = 'kelly2021374@concordiashanghai.org';
 
-$lab_monitors_email = 'Nicholas2019108@concordiashanghai.org';
+$game_development_email = 'cyrus2021262@concordiashanghai.org';
+$game_development_email2 = 'matthew2021313@concordiashanghai.org';
 
-$game_development_email = 'Nicholas2019108@concordiashanghai.org';
+$machine_learning_email = 'robert2022362@concordiashanghai.org';
+$machine_learning_email2 = 'charles2021407@concordiashanghai.org';
 
-$producers_email = 'Nicholas2019108@concordiashanghai.org';
-
-$music_composition_email = 'Nicholas2019108@concordiashanghai.org';
+$music_composition_email = 'kevin2021392@concordiashanghai.org';
 
 //literally everyone
 
-$literally_everyone_email = 'Nicholas2019108@concordiashanghai.org';
-
+$literally_everyone_email = 'cyrus2021262@concordiashanghai.org';
+$literally_everyone_email2 = 'alan2021233@concordiashanghai.org';
 
 
 ?>
