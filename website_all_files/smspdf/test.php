0dc55173e58c627d6f83c18b03d925605c39d8cb
Alan <alan2021233@users.noreply.github.com>
2021-04-06 13:52:11 +0800

    First page of automatic certificate pdf creator which takes service hours from database

---
+++ website_all_files/smspdf/test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- <link href="//db.onlinewebfonts.com/c/de2667f342f093d9526e2b71283629b2?family=Hobo+Std" rel="stylesheet" type="text/css"/> -->
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+  </head>
+  <body>
+    <h1 class="hobostd">Test test</h1>
+  </body>
+</html>
